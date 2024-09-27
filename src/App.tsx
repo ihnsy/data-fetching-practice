b0c010266db8b26d08cda2d4571db7c1d7835d64
ihnsy <snu1127@snu.ac.kr>
2024-09-27 20:22:42 +0900

    Revert "초안"

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,9 @@
 import './reset.css';
 
-import React, { useEffect, useState } from 'react';
+import { useReducer } from 'react';
 
-import { Post } from './interface';
-import Left from './props/Left';
-import Right from './props/Right';
+export const App = () => {
+  const [count, increment] = useReducer((c: number) => c + 1, 0);
 
-const App: React.FC = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [selectedPostId, setSelectedPostId] = useState<number | null>(null);
-
-  useEffect(() => {
-    // 포스트 목록을 가져오는 함수
-    const fetchPosts = async () => {
-      const response = await fetch(
-        'https://jsonplaceholder.typicode.com/posts',
-      );
-      const data = await response.json();
-      setPosts(data);
-      setSelectedPostId(data[0]?.id || null);
-    };
-
-    fetchPosts();
-  }, []);
-
-  return (
-    <div style={{ display: 'flex' }}>
-      <div style={{ width: '40%' }}>
-        <Left
-          posts={posts}
-          selectedPostId={selectedPostId}
-          onSelectPost={setSelectedPostId}
-        />
-      </div>
-      <div style={{ width: '60%' }}>
-        {selectedPostId && <Right postId={selectedPostId} />}
-      </div>
-    </div>
-  );
+  return <button onClick={increment}>{count}</button>;
 };
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
-// src/main.tsx
-
-import React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App'; // default import
+import { App } from './App';
 
 const root = document.getElementById('root');
-createRoot(root!).render(<App />);
+
+if (root === null) throw new Error('Root element not found');
+
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
