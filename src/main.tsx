d889250b1d3fd297ba6fdf8fc03a4c981e1da3e8
ihnsy <snu1127@snu.ac.kr>
2024-10-05 19:29:25 +0900

    main.tsx revised

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App'; // default import
+import App from './App.tsx';
 
 const root = document.getElementById('root');
-createRoot(root!).render(<App />);
+
+if (root === null) throw new Error('Root element not found');
+
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
